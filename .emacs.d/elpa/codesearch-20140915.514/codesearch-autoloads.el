63aea6d95c5e31b9aeac2dc2935aa20fa4bc0c95
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2014-09-21 21:28:42 -0700

    Sync ELPA

---
+++ .emacs.d/elpa/codesearch-20140915.514/codesearch-autoloads.el
@@ -0,0 +1,40 @@
+;;; codesearch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (codesearch-update-index codesearch-build-index
+;;;;;;  codesearch-search) "codesearch" "codesearch.el" (21528 20618
+;;;;;;  0 0))
+;;; Generated autoloads from codesearch.el
+
+(autoload 'codesearch-search "codesearch" "\
+
+
+\(fn PATTERN FILE-PATTERN)" t nil)
+
+(autoload 'codesearch-build-index "codesearch" "\
+Scan DIR to rebuild an index.
+
+\(fn DIR)" t nil)
+
+(autoload 'codesearch-update-index "codesearch" "\
+Update an existing index.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("codesearch-pkg.el") (21528 20618 440000
+;;;;;;  0))
+
+;;;***
+
+(provide 'codesearch-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; codesearch-autoloads.el ends here
