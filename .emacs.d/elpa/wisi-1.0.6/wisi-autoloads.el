a2101a9c18eb35a676be5a52ef277c24570a4c47
Cesar A. Quiroz, Ph.D <cesar_quiroz@ieee.org>
2014-09-29 23:28:38 -0700

    Sync ELPA (+ada-ref-man 2012.0)

---
+++ .emacs.d/elpa/wisi-1.0.6/wisi-autoloads.el
@@ -0,0 +1,19 @@
+;;; wisi-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("wisi-compat-24.2.el" "wisi-compile.el"
+;;;;;;  "wisi-parse.el" "wisi-pkg.el" "wisi.el") (21545 36092 689000
+;;;;;;  0))
+
+;;;***
+
+(provide 'wisi-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; wisi-autoloads.el ends here
